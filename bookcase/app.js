32372fbe024df602d97636da31a5dc499a888d75
zenjack <zenjack@foxmail.com>
2016-06-01 21:36:18 +0800

    添加導行

--- bookcase/app.js
+++ bookcase/app.js
@@ -14,7 +14,7 @@
 		author: '釋伽牟尼',
 		intro: '引領人進入真如世界，得大自在，無有煩惱之第一經典。為最上乘上人說，為大根大智人說。',
 		reviewFlag: true,
-		price: '10',
+		price: 10,
 		registerDate: new Date('2015-02-25'),
 		cover: 'lib/image/sjmn.jpg',
 		kind: '思想',
@@ -22,9 +22,9 @@
 	{
 		name: '道德經',
 		author: '老子',
-		intro: '揭示天地運行之基本法則，闡述了人何以與自然相處，何以做到無為而無不為。',
+		intro: '揭示天地運行之基本法則，闡述了人何以與自然相處，何以做到無為而無不為。	',
 		reviewFlag: true,
-		price: '11',
+		price: 11,
 		registerDate: new Date('2014-12-21'),
 		cover: 'lib/image/lz.jpg',
 		kind: '思想',
@@ -34,7 +34,7 @@
 		author: '孔子',
 		intro: '全面論述了人與人之間的相處倫理。',
 		reviewFlag: false,
-		price: '9',
+		price: 9,
 		registerDate: new Date('2016-05-01'),
 		cover: 'lib/image/kz.jpg',
 		kind: '思想',
@@ -42,7 +42,10 @@
 	
 	];
 
+	var tabFlag = 1;
+	
 	app.controller('BookListCtrl', function() {
 		this.books = books;
+		this.tabFlag = tabFlag;
 	});
 })();
